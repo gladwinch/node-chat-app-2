d1cec5d948e99b6443370326d1c270be3686d58f
Gladwin Chhinchni <gladwinch@gmail.com>
2019-03-08 09:34:28 +0530

    third commit

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
+const socketIO = require('socket.io')
 
-const app = express()
-const port = process.env.PORT || 3000;
+let app = express()
+let port = process.env.PORT || 3000
+let server = http.createServer(app)
+let io = socketIO(server)
 
 const publicPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicPath))
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected 12')
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected')
+    })
+})
+
+server.listen(port, () => {
     console.log(`server is started on port ${port}`)
 })
 
